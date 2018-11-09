7b09088d2e93db923c46e3480bfe432920b35142
Murali Vijayaraghavan <vmurali@sifive.com>
2018-11-08 23:25:05 -0800

    wrote the modified sqrt algo to match with div

--- src/main/scala/DivSqrtRecFN_small.scala
+++ src/main/scala/DivSqrtRecFN_small.scala
@@ -47,7 +47,7 @@ Division:
 Normal
 ------
 
-(qi + ci * 2^(-1))*2b <= 2a
+(qi + ci * 2^(-i))*2b <= 2a
 q0 = 0
 r0 = 2a
 
@@ -66,7 +66,8 @@ If (a < b), then we need to calculate (s+2)th bit and summary_(i+1)
 because we need s bits ignoring the leading zero. (This is skipCycle2
 part of Hauser's code.)
 
-Hauser:
+Hauser
+------
 sig_i = qi*2^(s+1)
 rem_i = 2^(i-1)*ri
 cycle_i = s+3-i
@@ -104,84 +105,59 @@ according to r(i+1)
 
 Square root:
 
-
 Normal
 ------
-(x + b)^2 <= inp
-x^2 + b * (2x + b) <= inp
--------------------------
-x_1^2 <= inp //x_1 <= pow2((s-1)/2)
-b_1 = pow2((s-1)/2)/2
-r_1 = inp - pow2(s-1)
-c_i = bi * (2*x_i + b_i) <= ri
-x_{i+1} = x_i + c_i * b_i
-r_{i+1} = r_i - b_i * (2*x_i + b_i)
-b_{i+1} = b_i/2 = b_1/pow2(i) = pow2((s-1)/2)/pow2(i+1)
------------------------------
-i = 1 to inf
-------------
+width of a is changed to (s+1)
+
+(xi + ci*2^(-i))^2 <= a
+xi^2 + ci*2^(-i)*(2xi+ci*2^(-i)) <= a
+x1 = 1
+ri = a - xi^2
+x(i+1) = xi + ci*2^(-i)
+r(i+1) = a - x(i+1)^2
+       = a - (xi^2 + ci*2^(-i)*(2xi+ci*2^(-i)))
+       = ri - ci*2^(-i)*(2xi+ci*2^(-i))
+       = ri - ci*2^(-i)*(2xi+2^(-i))  // ci is always 0 or 1
+ci = ri >= 2^(-i)*(2xi + 2^(-i))
+summary_i = ri != 0
+
+i = 1 to s+1
+
+(s+1)th bit plus summary_(i+1) gives enough information for rounding.
+
+Hauser
+------
+sig_i = xi
+rem_i = ri*2^(i-1)
+cycle_i = s+2-i
+bit_i = 2^(-i) (= 2^(s-i) = 2^(cycle_i-2) in terms of bit representation)
+
+sig_1 = 1 (= 2^s in terms of bit representation)
+rem_1 = a - 1 (= a - 2^s in terms of bit representation)
+cycle_1 = s+1
+bit_1 = 2^(-1) (= 2^(s-1) in terms of bit representation)
+
+ci = 2*sig_i + bit_i <= 2*rem_i
+ci = 2xi + 2^(-i) <= ri*2^i. Qed
+
+sig(i+1) = sig_i + ci * bit_i
+x(i+1) = xi + ci*2^(-i). Qed
+
+rem(i+1) = 2rem_i - ci*(2sig_i + bit_i)
+r(i+1)*2^i = ri*2^i - ci*(2xi + 2^(-i))
+r(i+1) = ri - ci*2^(-i)*(2xi + 2^(-1)). Qed
+
+For odd expression, do 2 steps initially.
+
+summary_1 = a <> 1 (= a <> 2^s in terms of bit representation)
+summary(i+1) = if ci then 2rem_i-(2sig_i+bit_i) <> 0 else summary_i, i <> 0
 
 
-Hauser's for even exp:
-----------------------
-inp = 1.xxxxxxxxx
-
-sig_i = x_i * b_i * pow2(i+1)
-bit_i = b_i^2 * (pow2(i+1))
-rem_i = r_i * pow2(i)
----------------------
-sig_1 <= pow2(s)
-bit_1 = pow2(s-1)
-rem_1 = 2*inp-pow2(s)
----------------------
-c_i = 2*sig_i + bit_i <= 2*rem_i
-sig_{i+1} = sig_i + c_i * bit_i
-rem_{i+1} = 2*rem_i - c_i*(2*sig_i + bit_i)
-bit_{i+1} = bit_i/2 = bit_1/pow2(i) = b_1^2*pow2(2)/pow2(i) = pow2((s-1)/2)/pow2(i)
--------------------------------------------------------------------------
-i = 1 to (s+1)
---------------
-At {s+1}
-sig_{s+1} = x_{s+1} * b_{s+1} * pow2(s+2) = x_{s+1} * pow2((s-1)/2) // Shifting by (s-1)/2 bits
-
-
-
-Hauser's for odd exp:
----------------------
-actualInp = 2*inp (as the remainder for the odd exponent when divided by 2 is observed by the mantissa)
-
-bit_1 = pow2(s-1)
-c_1 = {inp[s-1:s-2]-1, inp[s-3:0], 3'b0} >= {3'b101, (s-1)'b0} <-> {inp, 3'b0} >= {4'b1001, (s-1)'b0}
-sig_2 = {1'b1, c_1, (s-1)'b0}
-rem_2 = {inp[s-1:s-2]-1, inp[s-3:0], 3'b0} - c_1 * {3'b101, (s-1)'b0}
----------------------------------------------------------------------
-i = 2 to s+1
-
-// One extra step is done in the beginning, that's all.
-
-----------------------------------
-sig_2 = sig_1 + c_1 * bit_1
-rem_2 = 2*rem_1 - c_1 * (2*sig_1 + bit_1)
-
-
-c_1 = 2*pow2(s) + pow2(s-1) <= 2*(2*(2*inp) - pow2(s)) <-> 4*pow2(s) + pow2(s-1) <= 4*(2*inp) <-> 9*pow2(s-1) <= 4*(2*inp) <-> {4'b1001, (s-1)'b0} <= 4*(inp * 2)
-sig_1 = pow2(s)
-rem_1 = 2*(2*inp) - pow2(s)
-bit_1 = pow2(s-1)
-
-sig_2 = pow2(s) + c_1 * pow2(s-1) = {1'b1, c_1, (s-1)'b0}
-rem_2 = 2*(2*(2*inp) - pow2(s)) - c_1 * (2*pow2(s) + pow2(s-1))
-      = 8*inp - 4*pow2(s-1) - c_1*(5*pow2(s-1))
-      = {inp[s-1:s-2]-1, inp[s-3:0], 3'b0} - c_1*{3'b101, (s-1)'b0}
-
-// Again, odd just does two steps instead of 1 in the beginning
-
-
-notZeroRem_Z is enough for rounding because Hauser calculates one extra bit and saves the rest, if non-zero, as 1 (using notZeroRem_Z). It works for every rounding mode.
-
-Note that all registers are updated normally until cycleNum == 2.
-At cycleNum == 2, rem_Z is not updated, but all other registers are updated normally.
-But, cycleNum == 1 does not read rem_Z to calculate anything, including notZeroRem_Z. So everything is fine.
+------------------------------
+Note that all registers are updated normally until cycle == 2.
+At cycle == 2, rem is not updated, but all other registers are updated normally.
+But, cycle == 1 does not read rem to calculate anything (note that final summary
+is calculated using the values at cycle = 2).
 
 */
 
